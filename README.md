0e34b9c1fd2420b44b5bd423920e1970000dbf21
Seth Feder <Seth@Seths-MacBook-Pro.local>
2018-02-13 17:00:01 -0600

    refined version

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # GiphyApi
+
+Link to site: https://sefeder.github.io/GiphyApi/
+
+![Screenshot of Cartoon Gifs Website](assets/images/CartoonScreenShot)
+
+This website allows users to click on one of the given cartoon buttons and will return a list of 10 gifs related to that cartoon from GIPHY. Additionally, users can add their own cartoon button and then have 10 gifs related to that cartoon returned. Upon clicking each gif, it will animate if it's still and it will become still if it's animated. The site utilizes ajax requests to GIPHY's API.
+
+Technology Used:
+
+HTML
+CSS/Bootstrap
+JavaScript
+JQuery
+AJAX Requests/APIs
+
+Future Versions:
+
+Future versions of this site will improve upon readability and styling and will add increased functionality to the returned gifs. 
--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,18 +1,29 @@
+
+// create a global variable that is an array with the starting buttons on it
 var cartoons = ['Recess', 'Spongebob', 'Doug', 'Hey Arnold', 'Catdog']
 
 $(document).ready(function () {
-
+// this function takes the user's input and adds it to the array above then renders buttons for each element in the array. it also clears the input once the submit button is clicked
     $("body").on("click", "#add-cartoon", function (event) {
         event.preventDefault();
         var cartoon = $("#cartoon-input").val().trim();
         cartoons.push(cartoon);
+        $('#cartoon-input').val('');
         renderButtons();
     });
+// this function allows the users to clear the buttons they've added
+    $("body").on("click", "#clear-cartoon", function (event) {
+        event.preventDefault();
+        cartoons = ['Recess', 'Spongebob', 'Doug', 'Hey Arnold', 'Catdog'];
+        renderButtons();
+    });
+
+  //this function uses a forloop to go through the array element by element and add each to a a button and append the button to the page  
     function renderButtons() {
         $("#buttons").empty();
         for (var i = 0; i < cartoons.length; i++) {
             var btn = $("<button>");
-            btn.addClass("cartoon-btn");
+            btn.addClass("cartoon-btn btn btn-primary");
             btn.attr("data-cartoon", cartoons[i]);
             btn.text(cartoons[i]);
             $("#buttons").append(btn);
@@ -20,10 +31,7 @@ $(document).ready(function () {
 
     };
     renderButtons();
-    
-
-
-
+// This function allows the user to click on one of the cartoon buttons and makes an ajax request to giphy's api inputing the text of the button they clicked to the url in the request 
     $("body").on("click",".cartoon-btn", function () {
         var selectCartoon = $(this).attr("data-cartoon");
         var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + selectCartoon + "&api_key=ZUNcGIXLV4h6Qw4f5VUBvA7buynArBZe&limit=10";
@@ -32,6 +40,7 @@ $(document).ready(function () {
             url: queryURL,
             method: "GET"
         })
+  // in this function the results of the api request (10 gifs) are loaded to the page      
             .then(function (response) {
                 var results = response.data;
                 $("#gifs-appear-here").empty();
@@ -52,7 +61,7 @@ $(document).ready(function () {
                     gifDiv.prepend(p);
                     $("#gifs-appear-here").prepend(gifDiv);
                 };
-           
+           // this function allows the user to click on the gif and switch it from its still state to its animated state and back. Both states are given in the api response.
                 $(".gif").on("click", function() {
                     var state = $(this).attr('data-state');
                     if (state === "still") {
